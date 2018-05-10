c7d2f9a17233a18f20c1a93f83fbe40792b785d8
Diego Trevino <diegotf30@gmail.com>
2018-05-10 06:42:19 -0500

    Added CPU-Process interaction
    Processes are now dynamically assigned place in a readyQueue and top of
    queue goes to CPU. In SRT, top of queue is constantly checked for shorter
    jobs

--- server.py
+++ server.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import socket
 import sys
-import time
 
 
 def setup() :
@@ -21,7 +20,7 @@ def setup() :
     return sock.accept()
 
 def receiveData() :
-    "Receives data from client, removes comments and formats it into a list"
+    "Receives data from client, removes comments and formats instruction into a list"
     data = connection.recv(256)
     print>> sys.stderr, 'server received "%s"' % data
     return data.split('/')[0].split()
@@ -32,42 +31,106 @@ def createProcess(readyQueue, process, pid) :
     readyQueue.append((pid, process[0], process[3], process[3]))
     readyQueue.sort(key=lambda tup: tup[3])
 
+def removeFromCPU(pid, CPU, CPU2, readyQueue) :
+    """Moves currently running process to readyQueue, this is
+        mostly a helper function for changeQueue() & killProcess()"""
+    if CPU and CPU[0] == pid :
+        readyQueue.append(CPU)
+        CPU = ()
+    elif CPU2 and CPU2[0] == pid :
+        readyQueue.append(CPU2)
+        CPU2 = ()
+    return CPU, CPU2
+
 def changeQueue(pid, removeFrom, addTo) :
     """Moves Process from CPU/readyQueue into blockedQueue (or vice versa)
-        because of I/O burst"""
-
+        because of an I/O burst"""
     process = [i for i in removeFrom if i[0] == pid][0]
     removeFrom = [i for i in removeFrom if i[0] != pid]
 
     addTo.append(process)
     addTo.sort(key=lambda tup: tup[3])
 
-def scheduleSRT(data) :
+def killProcess(pid, readyQueue, blockedQueue) :
+    process2kill = [i for i in readyQueue if i[0] == pid]
+    if process2kill :
+        readyQueue = [i for i in readyQueue if i[0] != pid]
+    else :  # Not found in readyQueue, process must be blocked
+        blockedQueue = [i for i in blockedQueue if i[0] != pid]
+
+    return process2kill if process2kill else [i for i in blockedQueue if i[0] == pid][0]
+
+def expropiateProcess(CPU, readyQueue) :
+    "If there's a shorter job in queue it is placed in CPU instead of the current one"
+    if CPU[3] > readyQueue[0][3] :
+        readyQueue.append(CPU)
+        return readyQueue.pop(0)
+    else :
+        return CPU
+
+def updateCPUStatus(CPU, CPU2, policy, time, readyQueue) :
+    """Substracts a second on CPUs running processes, otherwise it assigns
+    them a process. And in SRT, it verifies if there's a shorter job"""
+    if CPU and CPU[3] > 0 :
+        CPU = CPU[:3] + (CPU[3] - 1,)
+        time += 1
+    else :
+        CPU = readyQueue.pop(0)
+
+    if CPU2 and CPU2[3] > 0 :
+        CPU2 = CPU2[:3] + (CPU2[3] - 1,)
+        time += 1
+    else :
+        CPU2 = readyQueue.pop(0)
+
+    if policy == 'SRT' :
+        CPU = expropiateProcess(CPU, readyQueue)
+        CPU2 = expropiateProcess(CPU2, readyQueue)
+
+    return CPU, CPU2, time
+
+
+def printProgress() :
+    print('ola')
+
+def scheduler(data, policy, num_cpus, time) :
     "Focuses on executing the process with the Shortest Remaining Time left"
     readyQueue = []
     blockedQueue = []
-    pid = 0
+    # Keeps track of last-assigned PID
+    PIDCounter = 0
+    # Process currently in CPU
+    CPU = ()
+    if num_cpus > 1 :
+        # Other process in 2nd CPU (max. # CPUs is 2)
+        CPU2 = ()
 
     while True:
+        pid = data[3]  # Helper
+
         if 'CREATE' in data :
-            createProcess(readyQueue, data, pid)
-            connection.sendall('process created with PID: ', pid)
-            pid += 1
+            createProcess(readyQueue, data, PIDCounter)
+            connection.sendall('process created with PID: ', PIDCounter)
+            PIDCounter += 1
 
         elif 'I/O' in data :
-            pid = data[3]
             if 'INICIA' in data :
+                CPU, CPU2 = removeFromCPU(pid, CPU, CPU2, readyQueue)
                 changeQueue(pid, readyQueue, blockedQueue)
                 connection.sendall('process #', pid, ' has entered I/O')
-            else :
+            else :  # TERMINA I/O
                 changeQueue(pid, blockedQueue, readyQueue)
                 connection.sendall('process #', pid, ' has successfully exited I/O')
 
-        data = receiveData()
+        elif 'KILL' in data :
+            CPU, CPU2 = removeFromCPU(pid, CPU, CPU2, readyQueue)
+            process = killProcess(pid, blockedQueue, readyQueue)
+            connection.sendall('process #', process[0], ' terminated. CPU time: ', process[2] - process[3])
 
-def scheduleSJF(data) :
-    "Executes the Shortest Job First (in non-expulsive manner)"
-    pass
+        CPU, CPU2, time = updateCPUStatus(CPU, CPU2, policy, readyQueue, time)
+        printProgress()
+
+        data = receiveData()
 
 
 if __name__ == '__main__':
@@ -83,19 +146,16 @@ if __name__ == '__main__':
 
     try:
         data = receiveData()
-        start = int(data[0])
+        time = int(data[0])
         if data:
-            if policy == 'SRT' :
-                scheduleSRT(data)
-            else :
-                scheduleSJF(data)
+            scheduler(data, policy, num_cpus, time)
         else:  # No data sent by client
             print>> sys.stderr, 'no data from', client_address
             connection.close()
             sys.exit()
     finally:
         # Clean up the connection
-        print>> sys.stderr, 'se fue al finally'
+        print>> sys.stderr, 'Conexion con cliente ha terminado'
         connection.close()
 
     sys.exit()
